f6cdfe2766c36b5b75db68c5242eb62888fae8b9
dgonza20 <38640894+dgonza20@users.noreply.github.com>
2019-12-15 20:39:34 -0500

    Create main.cpp

---
+++ EightQueens/main.cpp
@@ -0,0 +1,19 @@
+// David Gonzales
+// Intro to AI-ITCS 3153
+// Project 1: Queens 8 Hill Climbing
+
+#include <iostream>
+#include "board.h"
+
+
+using namespace std;
+
+int main() {
+    
+    Board Game; 
+    
+    Game.begin();
+    
+
+    return 0;
+}
